87608584fe7cd8d152a4507e34d4e38af6638752
komoriyuichiro <thekomori1113@gmail.com>
2019-09-04 18:37:18 +0900

    wip: Add 'fs', read file

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const program = require("commander");
+const fs = require("fs");
+
+program.parse(process.argv);
+const filePath = program.args[0];
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+})
